a029d3855af2c2e0cef265fbff35eb60d3eddfa3
Ben <thisbenrogers@gmail.com>
2019-09-30 16:46:29 -0500

    :construction: Adds endpoints
    No logic in endpoints

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // implement your API here
+const express = require('express');
+
+const userModel = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+
+})
+
+server.get('/api/users', (req, res) => {
+
+})
+
+server.get('/api/users/:id', (req, res) => {
+
+})
+
+server.delete('/api/users/:id', (req, res) => {
+
+})
+
+server.put('/api/users/:id', (req, res) => {
+    
+})
