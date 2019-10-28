539a375c48d69d0b91e9d21b36d6a2078e250901
Ben <thisbenrogers@gmail.com>
2019-10-28 13:34:36 -0500

    :sparkles: Adds express
    Starts new server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+const port = 6669;
+server.listen(port, () => console.log(`\n<<< Server Running on Port ${port} >>>\n`))
