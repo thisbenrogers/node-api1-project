c835b4ced90b310bc2715fefc36d9b1a072b8627
Ben <thisbenrogers@gmail.com>
2019-10-28 13:39:10 -0500

    :sparkles: Uses JSON

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const express = require('express');
 const db = require('./data/db');
 const server = express();
 
+
+server.use(express.json());
+
+
 const port = 6669;
 server.listen(port, () => console.log(`\n<<< Server Running on Port ${port} >>>\n`))
